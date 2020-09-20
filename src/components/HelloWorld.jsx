9589f13af268ab9f540f373102abfa7db4cf8880
Kevin <kevinchacong95@gmail.com>
2020-09-20 04:08:44 -0300

    ESLint

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../assets/styles/App.scss'
+
+class HelloWorld extends React.Component {
+    render(){
+        return <h1>Hello World</h1>
+    }
+}
+
+export default HelloWorld
